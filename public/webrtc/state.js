0b237063f410818932b2bc66e93085f34bec935f
vipin <vy879736@gmail.com>
2026-03-09 20:32:23 +0530

    chore(callState): callState obj created to store globalState .

---
+++ public/webrtc/state.js
@@ -0,0 +1,42 @@
+
+
+ const callState = {
+  localStream:null,
+  peers:{},
+  remoteStreams:{},
+  dataChannels:{},
+  candidateQueue:{},
+  
+
+};
+
+
+export default callState;
+
+
+
+
+
+
+export const getPeerObj = (socketId)=>{
+    if(peers[socketId]){
+        return peers[socketId];
+    }else{
+        return null;
+    }
+}
+
+
+export const getRemoteStream = (socketId)=>{
+
+}
+
+
+// peers: {
+//     'B': {
+//         connection: RTCPeerConnection,
+//         remoteStream: MediaStream,
+//         username: 'Rahul'
+//     }
+// }
+// Ek jagah sab kuch, getter bhi simple
