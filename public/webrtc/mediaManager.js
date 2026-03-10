a7bcc22deb148cfc36e5188cf56b9484565b4d03
vipin <vy879736@gmail.com>
2026-03-10 11:04:28 +0530

    chore(mediaStart): startMedia defined and export to get users vidoe and audio streams.

---
+++ public/webrtc/mediaManager.js
@@ -0,0 +1,41 @@
+import callState from "./state.js"
+
+
+
+
+
+
+
+//Function To Start Media stream
+export const startMedia = async()=>{
+
+    try{
+      const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                width: { min: 640, ideal: 1280, max: 1920 },
+                height: { min: 480, ideal: 720, max: 1080 },
+                frameRate: { ideal: 30, max: 60 },
+                facingMode: "user",
+            },
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                autoGainControl: true,
+                sampleRate: 48000,
+            },
+        });
+
+   
+        if(!stream){
+            console.warn("mediaStreawm is not found");
+            return;
+        }
+        
+        callState.localStream = stream;
+        console.log("mediaStream accessed: ",callState.localStream);
+        console.log("localTracks: ",callState.localStream.getTracks());
+
+    }catch(err){
+        console.error(err);
+    }
+}
